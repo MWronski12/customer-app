7baefbfd36bd2aec22aeaeeffde582e075dca919
MWronski12 <wronski.mikolaj@gmail.com>
2022-06-04 14:43:15 +0100

    Fragments navigation setup, google maps api working, TODO resolve null and repsonse data class

--- app/src/main/java/com/example/disago_customer/MainActivity.kt
+++ app/src/main/java/com/example/disago_customer/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.disago_customer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("DEBUG", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("DEBUG", "Error getting documents.", exception)
+            }
     }
 }
